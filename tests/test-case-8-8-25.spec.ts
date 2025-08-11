b43c65e0a1b35e825b6e7ce4ebc1a7b0f42328ad
kishore kumaran <142519697+kishoreDev0@users.noreply.github.com>
2025-08-11 11:17:48 +0530

    test case added

--- tests/test-case-8-8-25.spec.ts
+++ tests/test-case-8-8-25.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('API Client Tests', () => { 
-    
+     
     test('Response Status on going to api',async({ page })=>{
         await page.goto('http://localhost:3000/api/users');
         const response = await page.waitForResponse('**/api/users');
         expect(response.status()).toBe(200);
     });
-    
-
+    test('Response Status on going to api with invalid token',async({ page })=>{
+        await page.goto('http://localhost:3000/api/users');
+        const response = await page.waitForResponse('**/api/users');
+        expect(response.status()).toBe(401);
+    });
 })
